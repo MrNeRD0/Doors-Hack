1d116b4dd7bd64ef3b3f1edf63ecf3b8392e4298
MrNeRD0 <126970039+MrNeRD0@users.noreply.github.com>
2023-03-27 15:53:35 +0200

    Update FullDoorsGuiHack_Done.lua

--- FullDoorsGuiHack_Done.lua
+++ FullDoorsGuiHack_Done.lua
@@ -1,12 +1,1 @@
--- create a ScreenGui to hold the label
-local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
--- create a TextLabel to display the text
-local textLabel = Instance.new("TextLabel")
-textLabel.Size = UDim2.new(1, 0, 1, 0) -- fill the entire screen
-textLabel.Text = "Please Check The Nerd Channel For Last Update(link)"
-textLabel.FontSize = Enum.FontSize.Size36 -- set the font size to be large
-textLabel.TextColor3 = Color3.new(1, 1, 1) -- set the text color to white
-textLabel.BackgroundTransparency = 1 -- make the background transparent
-textLabel.Parent = screenGui
